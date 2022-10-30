358f97b76b6fab88a1012eea3ed5d5c340ec1bd8
scentille-bok <scentille-bok@users.noreply.github.com>
2022-10-29 19:16:35 -0500

    sc - Refactoring the Cybercrimes List with Joshua.

--- src/main/java/edu/nsu/cis/model/db/Cybercrime.java
+++ src/main/java/edu/nsu/cis/model/db/Cybercrime.java
@@ -1,11 +1,18 @@
 package edu.nsu.cis.model.db;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+
+@Setter
+@Getter
 @Entity
 @Table(name = "cybercrime", schema = "dbo")
 public class Cybercrime {
@@ -27,36 +34,8 @@ public class Cybercrime {
     @OneToMany(mappedBy = "cybercrimeID")
     private Set<Cybercrimes> cybercrimes = new LinkedHashSet<>();
 
-    public Integer getId() {
-        return id;
+    @Override
+    public String toString() {
+        return String.valueOf(this.id);
     }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCybercrimeType() {
-        return cybercrimeType;
-    }
-
-    public void setCybercrimeType(String cybercrimeType) {
-        this.cybercrimeType = cybercrimeType;
-    }
-
-    public String getSeverityLevel() {
-        return severityLevel;
-    }
-
-    public void setSeverityLevel(String severityLevel) {
-        this.severityLevel = severityLevel;
-    }
-
-    public Set<Cybercrimes> getCybercrimes() {
-        return cybercrimes;
-    }
-
-    public void setCybercrimes(Set<Cybercrimes> cybercrimes) {
-        this.cybercrimes = cybercrimes;
-    }
-
 }
