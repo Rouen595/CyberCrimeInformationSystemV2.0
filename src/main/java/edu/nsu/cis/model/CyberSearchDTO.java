67d2f1312ba3214e3a2de84942e518290cf8b737
scentille-bok <scentille-bok@users.noreply.github.com>
2022-10-30 15:13:45 -0500

    sc - Misc. Fixes and refactoring.

---
+++ src/main/java/edu/nsu/cis/model/CyberSearchDTO.java
@@ -0,0 +1,16 @@
+package edu.nsu.cis.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CyberSearchDTO {
+
+  private String cybercrimeType;
+  private String severityLevel;
+
+
+}
