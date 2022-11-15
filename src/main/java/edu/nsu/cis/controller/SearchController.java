862ac42691449df16d43a421a00638c18b1a230b
Rouen595 <76932810+Rouen595@users.noreply.github.com>
2022-11-15 17:00:56 -0600

    Joshua: Exapnding main search page to include more options

--- src/main/java/edu/nsu/cis/controller/SearchController.java
+++ src/main/java/edu/nsu/cis/controller/SearchController.java
@@ -2,8 +2,15 @@ package edu.nsu.cis.controller;
 
 import edu.nsu.cis.model.CyberSearchDTO;
 import edu.nsu.cis.model.db.Cybercrime;
+import edu.nsu.cis.model.db.Cybercrimes;
+import edu.nsu.cis.model.db.Person;
+import edu.nsu.cis.model.db.Persontype;
 import edu.nsu.cis.service.CybercrimeService;
+import edu.nsu.cis.service.CybercrimesService;
+import edu.nsu.cis.service.PersonService;
+import edu.nsu.cis.service.PersonTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +23,15 @@ public class SearchController {
     @Autowired
     private CybercrimeService cybercrimeService;
 
+    @Autowired
+    private CybercrimesService cybercrimesService;
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PersonTypeService personTypeService;
+
     /*
     @GetMapping("/search")
     public String searchCybercrime(Model model) {
@@ -27,11 +43,18 @@ public class SearchController {
     @GetMapping("/search")
     public String searchAll(Model model) {
         List<Cybercrime> cybercrimeList = cybercrimeService.retrieveAll();
+        List<Cybercrimes> cybercrimesList = cybercrimesService.retrieveAll();
         List<String> cybercrimeBySeverityLevelList = cybercrimeService.retrieveDistinctBySeverityLevel();
+        List<Person> personList = personService.retrieveAll();
+        List<Persontype> persontypeList = personTypeService.retrieveAll();
 
         model.addAttribute("cyberSearch", new CyberSearchDTO());
         model.addAttribute("cybercrimeList", cybercrimeList);
+        model.addAttribute("cybercrimesList", cybercrimesList);
         model.addAttribute("cybercrimeBySeverityLevelList", cybercrimeBySeverityLevelList);
+        model.addAttribute("personList", personList);
+        model.addAttribute("persontypeList", persontypeList);
+
         return "/search";
     }
 
