2867ae0665dfc2a01530e724e65f6cda7194a59a
scentille-bok <scentille-bok@users.noreply.github.com>
2022-11-21 20:42:39 -0600

    sc - [integration] Fixed NumberFormatException when Search Parameters does not include CyberCrime.

--- src/main/java/edu/nsu/cis/controller/SearchController.java
+++ src/main/java/edu/nsu/cis/controller/SearchController.java
@@ -9,6 +9,7 @@ import edu.nsu.cis.service.CybercrimeService;
 import edu.nsu.cis.service.CybercrimesService;
 import edu.nsu.cis.service.PersonService;
 import edu.nsu.cis.service.PersonTypeService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -74,7 +75,8 @@ public class SearchController {
         model.addAttribute("cyberSearch", cyberSearch);
 
         List<Cybercrimes> cybercrimesList = cybercrimesService.searchCybercrimes(
-                Integer.parseInt(cyberSearch.getCybercrimeType()), cyberSearch.getSeverityLevel(), cyberSearch.getPunishment(), cyberSearch.getArrestDate(),
+                StringUtils.isNotBlank(cyberSearch.getCybercrimeType()) ? Integer.parseInt(cyberSearch.getCybercrimeType()) : 0,
+                cyberSearch.getSeverityLevel(), cyberSearch.getPunishment(), cyberSearch.getArrestDate(),
                 cyberSearch.getSentencingDate(), cyberSearch.getFirstName(), cyberSearch.getLastName(), cyberSearch.getStreetAddress(), cyberSearch.getCity(), cyberSearch.getState(),
                 cyberSearch.getZipcode());
 
